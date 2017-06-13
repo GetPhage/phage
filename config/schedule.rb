3d2cba2ebdc5186c63abde0f47c27a1b642d0f94
John Romkey <romkey@romkey.com>
2017-06-13 12:22:24 -0700

    fixed action mailer body in config/locales/en.yml
    changed schedule.rb to properly work with rbenv

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,11 @@
+job_type :rbenv_rake, %Q{export PATH=/home/phage/.rbenv/shims:/home/phage/.rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)";  cd :path && :environment_variable=:environment :bundle_command rake :task --silent :output }
+
 every 1.minutes do
-  rake "passive:scan"
+  rbenv_rake "passive:scan"
 end
 
 every 5.minutes do
-  rake "mdns:scan"
+  rbenv_rake "mdns:scan"
 end
 
 #every 5.minutes do
