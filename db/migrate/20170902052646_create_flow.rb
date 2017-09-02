c12ef6da1d95e2f02e27c6640677da4137f23c16
John Romkey <romkey@romkey.com>
2017-09-01 22:30:48 -0700

    add flows to the database

---
+++ db/migrate/20170902052646_create_flow.rb
@@ -0,0 +1,23 @@
+class CreateFlow < ActiveRecord::Migration[5.0]
+  def change
+    create_table :flows do |t|
+      t.belongs_tos :device
+      t.macaddr :mac_address, null: false
+      t.inet :src_ip, null: false, default: '0.0.0.0'
+      t.inet :dst_ip, null: false, default: '0.0.0.0'
+      t.integer :src_port, null: false, default: 0
+      t.integer :dst_port, null: false, default: 0
+      t.string :hostname, null: false, default: ''
+      t.integer :duration, null: false, default: 0
+      t.integer :bytes_sent, null: false, default: 0
+      t.string :bytes_received, null: false, default: 0
+    end
+    add_index :flows, :src_ip
+    add_index :flows, :dst_ip
+    add_index :flows, :src_port
+    add_index :flows, :dst_port
+    add_index :flows, :hostname
+    add_index :flows, :duration
+    add_index :flows, :bytes_received
+  end
+end
