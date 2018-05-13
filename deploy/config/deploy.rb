b4baf45cba2e3e00bb254bb318119ee69173cac1
John Romkey <romkey@romkey.com>
2018-05-12 22:21:58 -0700

    bump Ruby version to 2.5.1
    update Capistrano gems and bump locked Capistrano version

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -1,11 +1,11 @@
 # config valid only for current version of Capistrano
-lock "3.8.1"
+lock "3.10.2"
 
 set :application, "phage"
 set :repo_url, "git@github.com:GetPhage/phage.git"
 set :deploy_to, '/home/phage/phage'
 
-set :rbenv_ruby, "2.3.1"
+set :rbenv_ruby, "2.5.1"
 
 set :bugsnag_api_key, ENV['BUGSNAG_API_KEY']
 
