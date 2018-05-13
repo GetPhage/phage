caeb84311d32109e538b219f426faaf630fffbc7
John Romkey <romkey@romkey.com>
2018-05-13 15:42:10 -0700

    new Ansible role to install phage virtual host
    tweak deploy to create needed shared directories

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -7,6 +7,11 @@ set :deploy_to, '/home/phage/phage'
 
 #set :rbenv_ruby, "2.5.1"
 
+#append :linked_files, 'config/master.key'
+
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", '.bundle'
+
+
 set :bugsnag_api_key, ENV['BUGSNAG_API_KEY']
 
 # Default branch is :master
