19fe8ec37bc13d575cbadf0f290b59c46172047c
John Romkey <romkey@romkey.com>
2018-05-14 20:59:28 -0700

    histories belong to devices and have optional users

--- app/models/device.rb
+++ app/models/device.rb
@@ -11,6 +11,7 @@ class Device < ApplicationRecord
   belongs_to :product, optional: true
   belongs_to :network, optional: true
   has_many :scan_diff, dependent: :destroy
+  has_many :history, dependent: :destroy
   has_many :flow
 
   scope :per_user, -> user { where(network: Network.per_user(user).first) }
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,6 +1,7 @@
 class History < ApplicationRecord
   belongs_to :scan_diff
-  belongs_to :user
+  belongs to :device
+  belongs_to :user, optional: true
 
   scope :per_user, -> (user) { where(user: user) }
 end
