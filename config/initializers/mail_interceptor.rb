379efcc822593edff9f3e563b8bc27b69af219cb
John Romkey <romkey@romkey.com>
2018-05-17 08:11:59 -0700

    fix tupo

--- config/initializers/mail_interceptor.rb
+++ config/initializers/mail_interceptor.rb
@@ -1,4 +1,4 @@
-class SandboxMailInterceptor
+class SandboxEmailInterceptor
   def self.delivering_email(message)
     File.open("/tmp/debug-email", "a") { |f| f.write(message); f.write("\n\n\n>>>>> END <<<<<\n"); }
   end
