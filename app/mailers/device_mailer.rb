a3ccf2c29a3ee21f887439734a882203261c5ea8
John Romkey <romkey@romkey.com>
2018-05-17 07:45:17 -0700

    another attempt to fix mail from:

--- app/mailers/device_mailer.rb
+++ app/mailers/device_mailer.rb
@@ -1,4 +1,5 @@
 class DeviceMailer < ApplicationMailer
+  default from: 'phage@romkey.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -8,6 +9,6 @@ class DeviceMailer < ApplicationMailer
   def new_device(device)
     @greeting = "Hi"
 
-    mail to: "romkey@romkey.com"
+    mail to: "romkey@romkey.com", subject: "Hello from Phage", from: "phage@romkey.com"
   end
 end
