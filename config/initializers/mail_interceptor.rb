cf065171a49a9bb3ab108fb8c4f0643eff7fd125
John Romkey <romkey@romkey.com>
2018-05-17 08:11:02 -0700

    debug email

---
+++ config/initializers/mail_interceptor.rb
@@ -0,0 +1,7 @@
+class SandboxMailInterceptor
+  def self.delivering_email(message)
+    File.open("/tmp/debug-email", "a") { |f| f.write(message); f.write("\n\n\n>>>>> END <<<<<\n"); }
+  end
+end
+
+ActionMailer::Base.register_interceptor(SandboxEmailInterceptor)
